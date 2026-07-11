f5c74ddf482fb0dd9b2b32402b4ea38cdb49b4c1
arnaudetitia <arnaud.etitia@gmail.com>
2026-07-11 15:54:03 +0200

    layout page remote

--- src/app/components/remote-page/remote-page.component.ts
+++ src/app/components/remote-page/remote-page.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import { MargeFormControlComponent } from './marge-form-control/marge-form-control.component';
+import { Marge } from '../../model/marge.model';
 
 @Component({
   selector: 'app-remote-page.component',
-  imports: [],
+  imports: [MargeFormControlComponent, MatSliderModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './remote-page.component.html',
   styleUrl: './remote-page.component.scss',
 })
 export class RemotePageComponent {
+  readonly MIN_YEAR = 1950;
+  readonly MAX_YEAR = new Date().getFullYear();
 
+  reponseForm: FormGroup;
+
+  reponseSent = signal<boolean>(false);
+
+  constructor(private formBuilder: FormBuilder) {
+    this.reponseForm = this.formBuilder.group({
+      marge: [null, [Validators.required]],
+      annee: [this.MIN_YEAR, [Validators.required]],
+    });
+  }
+
+  modifierAnnee(increment: number) {
+    const currentAnnee = this.reponseForm.get('annee')?.value;
+    this.reponseForm.patchValue({ annee: currentAnnee + increment });
+  }
+
+  sendMarge(marge: Marge | null) {
+    this.reponseForm.patchValue({ marge: marge });
+  }
+
+  confirmerReponse() {
+    this.reponseSent.set(true);
+  }
 }
