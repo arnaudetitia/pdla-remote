aa69b24dc4b817a82f2b0741a281c049fa900c11
arnaudetitia <arnaud.etitia@gmail.com>
2026-07-14 12:36:26 +0200

    retouches de la remote

--- src/app/components/remote-page/marge-form-control/marge-form-control.component.ts
+++ src/app/components/remote-page/marge-form-control/marge-form-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, forwardRef, Output, signal } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, Output, signal } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Marge } from '../../../model/marge.model';
 import { CommonModule } from '@angular/common';
@@ -16,6 +16,8 @@ import { CommonModule } from '@angular/common';
   styleUrl: './marge-form-control.component.scss',
 })
 export class MargeFormControlComponent implements ControlValueAccessor {
+  @Input() reponseSent: boolean = false;
+
   @Output() onMargeChanged = new EventEmitter<Marge | null>();
 
   margesList: Marge[] = [
@@ -37,21 +39,34 @@ export class MargeFormControlComponent implements ControlValueAccessor {
     },
   ];
   value = signal<Marge | null>(null);
-  onChange = (val: number[]) => {};
+  disabled = signal<boolean>(false);
+  onChange = (val: Marge | null) => {};
   onTouched = () => {};
-  writeValue(marge: Marge): void {
-    if (marge) {
-      this.value.set(marge);
-      this.onMargeChanged.emit(this.value());
-    }
+
+  writeValue(marge: Marge | null): void {
+    this.value.set(marge);
   }
+
+  selectMarge(marge: Marge): void {
+    if (this.reponseSent || this.disabled()) {
+      return;
+    }
+
+    this.value.set(marge);
+    this.onChange(marge);
+    this.onTouched();
+    this.onMargeChanged.emit(marge);
+  }
+
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
+
   registerOnTouched(fn: any): void {
     this.onTouched = fn;
   }
-  setDisabledState?(isDisabled: boolean): void {
-    throw new Error('Method not implemented.');
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
   }
 }
